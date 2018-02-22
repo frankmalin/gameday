95a2c16422f663a0b10019af6daee0997c12ac2c
Frank Malin <malin@us.ibm.com>
2018-02-22 07:45:52 -0600

    Testing update

--- bin/00_utilities.sh
+++ bin/00_utilities.sh
@@ -130,6 +130,16 @@ function buildevent()
 	echo $event: $what @$time
 }
 
+function buildevent()
+{
+	# Build an event for logging
+	local event=$1
+	local time=$2
+	local what="$3"
+
+	echo $event: $what @$time
+}
+
 function gettime()
 {
 	egrep $timestr $timefile | cut -f2 -d:
