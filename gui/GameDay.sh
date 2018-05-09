592389e4995de952a223858c8a02079953f23ff8
Frank Malin <frank.malin@gmail.com>
2018-05-09 08:34:04 -0500

    Make log

--- gui/GameDay.sh
+++ gui/GameDay.sh
@@ -3,5 +3,5 @@
 # Starter shell to allow for file redirect
 #
 # 
-[[ -f ../log/GameDay.Gui.log ]] && mv ../log/GameDay.Gui.log ../log/GameDay.Gui.log.`date | tr ' ' '_' | tr ':' '-'`
+[[ -d ../log/ ]] && { mv ../log ../log-`date | tr ' ' '_' | tr ':' '-'` ; mkdir ../log ; }
 python ./GameDay.py 2>&1 | tee ../log/GameDay.Gui.log
